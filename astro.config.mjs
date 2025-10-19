d0035b5683dbc1489d8bfe9ca8d74aa98c1d668b
reniel0827 <renielvillegas@gmail.com>
2025-10-20 07:17:31 +0800

    October 20 commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,12 @@
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 import icon from "astro-icon";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+    vite: {
+    plugins: [tailwindcss()],
+  },
   site: "https://www.yourwebsite.com", // update me!
   integrations: [
     icon(),
